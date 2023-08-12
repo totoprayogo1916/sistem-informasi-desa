f2fd17152540c78e6c8226d2c3b4e2ebc1d300fc
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-08-12 12:27:14 +0700

    migrate: analisis_kategori_indikator

--- donjo-app/Database/Migrations/2023-08-12-043927_Init.php
+++ donjo-app/Database/Migrations/2023-08-12-043927_Init.php
@@ -27,10 +27,24 @@ class Init extends Migration
         $this->forge->addKey('id_kategori');
 
         $this->forge->createTable('analisis_indikator', true);
+
+        // analisis_kategori_indikator
+        $this->forge->addField([
+            'id'            => ['type' => 'TINYINT', 'constraint' => 4, 'auto_increment' => true],
+            'id_master'     => ['type' => 'TINYINT', 'constraint' => 4],
+            'kategori_kode' => ['type' => 'VARCHAR', 'constraint' => 3],
+            'kategori'      => ['type' => 'VARCHAR', 'constraint' => 50],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addKey('id_master');
+
+        $this->forge->createTable('analisis_kategori_indikator', true);
     }
 
     public function down()
     {
         $this->forge->dropTable('analisis_indikator');
+        $this->forge->dropTable('analisis_kategori_indikator');
     }
 }
